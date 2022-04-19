d1c86d54f498fca166e957d249b109645ea13627
rasd3 <yckim@spa.hanyang.ac.kr>
2022-04-19 07:39:26 +0000

    [mod] sex

--- mmdet3d/models/model_utils/actr_transformer.py
+++ mmdet3d/models/model_utils/actr_transformer.py
@@ -425,9 +425,10 @@ class DeformableTransformerEncoder(nn.Module):
                     encoder_layer.d_model,
                     encoder_layer.d_model,
                     num_layers=lt_cfg.num_layers,
-                    attn_feat_agg_method=lt_cfg.attn_feat_agg_method,
-                    feat_agg_method=lt_cfg.feat_agg_method
-                ), num_layers)
+                    attn_feat_agg_method=lt_cfg.get('attn_feat_agg_method',
+                                                    'unique'),
+                    feat_agg_method=lt_cfg.get('feat_agg_method', 'replace')),
+                num_layers)
 
     @staticmethod
     def get_reference_points(spatial_shapes, valid_ratios, device):
